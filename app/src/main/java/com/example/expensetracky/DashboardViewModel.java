2c2bb8730f0c36fcd0700144bbb5a3424a4dab34
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-09-28 18:35:48 +0530

    Implement functionality of Load transaction and plot Amount and category on pie chart

--- app/src/main/java/com/example/expensetracky/DashboardViewModel.java
+++ app/src/main/java/com/example/expensetracky/DashboardViewModel.java
@@ -1,4 +1,4 @@
-package com.example.expensetracky.ui.dashboard;
+package com.example.expensetracky;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
--- app/src/main/java/com/example/expensetracky/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/expensetracky/ui/dashboard/DashboardFragment.java
@@ -1,4 +1,4 @@
-package com.example.expensetracky;
+package com.example.expensetracky.ui.dashboard;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.expensetracky.DashboardViewModel;
 import com.example.expensetracky.databinding.FragmentDashboardBinding;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
